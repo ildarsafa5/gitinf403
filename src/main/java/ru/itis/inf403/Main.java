08f9ec23678197fc9d0e2427147df6c124c71b7b
ildarsafa5 <ildarsafa5@gmail.com>
2024-12-04 10:43:06 +0300

    deleted

--- src/main/java/ru/itis/inf403/Main.java
+++ src/main/java/ru/itis/inf403/Main.java
@@ -6,7 +6,6 @@ public class Main {
         System.out.println("Hello  World");
         System.out.println("Hello  World");
         System.out.println();
-        System.out.println();
 
     }
 }
